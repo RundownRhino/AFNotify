663a5fe33baca5b4e51d53721ce51537781a8db1
RundownRhino <52856631+RundownRhino@users.noreply.github.com>
2021-03-24 18:53:50 +0300

    Make notifications optional, .get() exception handling

--- AFNotify.py
+++ AFNotify.py
@@ -3,16 +3,18 @@ import logging
 import time
 import sys
 
-# noinspection Mypy
-from pynotifier import Notification
-from requests import get
+from requests import get, RequestException
 
 status_links = {"eu": r"https://eu.acolytefight.io/status", "us": r"https://us.acolytefight.io/status"}
 request_period = 300  # seconds (5 minutes)
+to_notify = True  # whether to enable desktop notifications
 notification_dur = 10  # seconds - note that the program can't be shut down when the notification is showing
 thresholds = {
     "eu": 4}  # how many players to notify about. Omit a server to ignore. No requests are made to ignored servers.
 log_all = True  # whether to log all the numbers
+
+if to_notify:
+    from pynotifier import Notification  # type:ignore
 
 # configure logging to log into a file and add some data
 logging.basicConfig(
@@ -48,7 +50,12 @@ sys.excepthook = handle_exception
 while True:
     counts = {}
     for key, link in status_links.items():
-        response = get(link)
+        # noinspection PyBroadException
+        try:
+            response = get(link)
+        except RequestException:
+            logging.exception(f"Failed getting a response from server {key}.")
+            continue
         # noinspection PyBroadException
         try:
             if response.status_code == 200:
@@ -62,13 +69,14 @@ while True:
     if log_all:
         logging.info(f"Fetched counts: {json.dumps(counts)}")
 
-    for k, count in counts.items():
-        if count >= thresholds[k]:
-            Notification(
-                title="AFNotify Alert",
-                description=f'{count} players are currently on {k.upper()}!',
-                duration=notification_dur,
-                urgency=Notification.URGENCY_NORMAL
-            ).send()
-            break  # don't send more than one notification, even if there's several servers
+    if to_notify:
+        for k, count in counts.items():
+            if count >= thresholds[k]:
+                Notification(
+                    title="AFNotify Alert",
+                    description=f'{count} players are currently on {k.upper()}!',
+                    duration=notification_dur,
+                    urgency=Notification.URGENCY_NORMAL
+                ).send()
+                break  # don't send more than one notification, even if there's several servers
     time.sleep(request_period)
