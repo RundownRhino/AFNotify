0830d52457d6ac2e2e0ecc516ba5eca8d5237878
RundownRhino <52856631+RundownRhino@users.noreply.github.com>
2021-03-24 18:19:15 +0300

    Autoformat

--- AFNotify.py
+++ AFNotify.py
@@ -2,19 +2,19 @@ from requests import get
 from pynotifier import Notification
 import logging, time
 
-status_links = {"eu":r"https://eu.acolytefight.io/status","us":r"https://us.acolytefight.io/status"}
-request_period = 300   # seconds (5 minutes)
-notification_dur = 10   # seconds - note that the program can't be shut down when the notification is showing
-thresholds = {"eu":4}  # how many players to notify about. Omit a server to ignore. No requests are made to ignored servers.
-log_all = True       # whether to log all the numbers
-
+status_links = {"eu": r"https://eu.acolytefight.io/status", "us": r"https://us.acolytefight.io/status"}
+request_period = 300  # seconds (5 minutes)
+notification_dur = 10  # seconds - note that the program can't be shut down when the notification is showing
+thresholds = {
+    "eu": 4}  # how many players to notify about. Omit a server to ignore. No requests are made to ignored servers.
+log_all = True  # whether to log all the numbers
 
 # configure logging to log into a file and add some data
 logging.basicConfig(
     format='%(asctime)s - %(message)s',
-    filemode = "a",
-    filename = "AFNotifyLog.txt",
-    level = logging.INFO # change to DEBUG for tons of debug stuff
+    filemode="a",
+    filename="AFNotifyLog.txt",
+    level=logging.INFO  # change to DEBUG for tons of debug stuff
 )
 # also log to console if run in it:
 logging.getLogger().addHandler(logging.StreamHandler())
@@ -22,7 +22,7 @@ logging.getLogger().addHandler(logging.StreamHandler())
 logging.critical("Process started.")
 
 # ignore servers with no thresholds:
-status_links = {k:v for k,v in status_links.items() if k in thresholds}
+status_links = {k: v for k, v in status_links.items() if k in thresholds}
 if not status_links:
     raise ValueError("Nothing to watch for!")
 
@@ -36,20 +36,20 @@ while True:
                 data = response.json()
                 count = data["numPlayers"]
                 counts[key] = count
-        except: # KeyError, most likely, but catch and log all
+        except:  # KeyError, most likely, but catch and log all
             logging.exception(f"Failed handling a response from server {key}.")
             logging.debug(f"Response content: {response.content}")
-            
+
     if log_all:
         logging.info(f"Fetched counts: {counts}")
-    
+
     for k, count in counts.items():
         if count >= thresholds[k]:
             Notification(
                 title="AFNotify Alert",
-                description = f'{count} players are currently on {k.upper()}!',
+                description=f'{count} players are currently on {k.upper()}!',
                 duration=notification_dur,
                 urgency=Notification.URGENCY_NORMAL
             ).send()
-            break # don't send more than one notification, even if there's several servers
+            break  # don't send more than one notification, even if there's several servers
     time.sleep(request_period)
